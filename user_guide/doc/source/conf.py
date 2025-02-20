10716cc5841bab804e8d8e6d1adafb1e9aa46fd1
Sam Clarke-Green <sam.clarke@metoffice.gov.uk>
2025-02-20 22:07:03 +0000

    Bump copyright year

--- user_guide/doc/source/conf.py
+++ user_guide/doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Joint UK Land Environment Simulator (JULES)'
-copyright = u'2023'
+copyright = u'2025'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
